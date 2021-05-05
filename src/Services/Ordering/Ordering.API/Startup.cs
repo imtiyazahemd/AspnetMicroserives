9a3b5673f1fd7ef9a9a036dd618e87528fd01ea2
imtiyaz ahmed <imtiyaz.n.ahmed@gmail.com>
2021-05-05 11:41:33 -0700

    Rabbit mq Complete

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -1,11 +1,15 @@
+using EventBus.Messages.Common;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Ordering.API.EventBusComsumer;
 using Ordering.Application;
 using Ordering.Infrastructure;
 using System;
@@ -29,6 +33,24 @@ namespace Ordering.API
         {
             services.AddApplicationServices();
             services.AddInfrastructureServices(Configuration);
+
+            // MassTransit-RabbitMQ Configuration
+            services.AddMassTransit(config => {
+
+                config.AddConsumer<BasketCheckoutConsumer>();
+                config.UsingRabbitMq((ctx, cfg) => {
+                    cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+                    // cfg.Host("amqp://guest:guest@localhost:5672");
+                    cfg.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue,c=> {
+                        c.ConfigureConsumer<BasketCheckoutConsumer>(ctx);                    
+                    });
+                });
+            });
+            services.AddMassTransitHostedService();
+
+            // General Configuration
+            services.AddAutoMapper(typeof(Startup));
+            services.AddScoped<BasketCheckoutConsumer>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
